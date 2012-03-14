4674667f8047e7827eda0a5992691deca622e8ac
wxianfeng <wang.fl1429@gmail.com>
2012-03-14 11:37:03 +0800

    modify father Class

--- app/controllers/simple_captcha_controller.rb
+++ app/controllers/simple_captcha_controller.rb
@@ -1,7 +1,8 @@
 # encoding:utf-8
-class SimpleCaptchaController < ActionController::Metal
-  include ActionController::Streaming if Rails.version.to_f < 3.1
-  include ActionController::DataStreaming if Rails.version.to_f >= 3.1
+# class SimpleCaptchaController < ActionController::Metal 
+class SimpleCaptchaController < ActionController::Base
+  # include ActionController::Streaming if Rails.version.to_f < 3.1
+  # include ActionController::DataStreaming if Rails.version.to_f >= 3.1
   include SimpleCaptcha::ImageHelpers
 
   # GET /simple_captcha
